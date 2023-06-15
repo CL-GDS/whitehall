1c9350f3af3fd740f172d9b4a7064f65767700bf
Conor <conor.lowney@digital.cabinet-office.gov.uk>
2023-06-15 10:27:35 +0100

    added multi_part model as sibling to detailed guide
    added NOTE on function that might need including later

---
+++ app/models/multi_part.rb
@@ -0,0 +1,38 @@
+class MultiPart < Edition
+
+  def self.format_name
+    "multi_part"
+  end
+
+  def rendering_app
+    Whitehall::RenderingApp::GOVERNMENT_FRONTEND
+  end
+
+  def rummager_index
+    :multi_parts
+  end
+
+  def display_type_key
+    "multi_part"
+  end
+
+  def search_format_types
+    super + [MultiPart.search_format_type]
+  end
+
+  def self.search_format_type
+    "multi_part"
+  end
+
+  def base_path
+    "/multi_part/#{slug}"
+  end
+
+  # NOTE: List below is things that could be needed in future
+
+  # Added as they need a Gov relation but keeping it slim at starting
+  # def government
+  #   @government ||= Government.on_date(date_for_government) unless date_for_government.nil?
+  # end
+
+end
